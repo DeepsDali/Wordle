7c275bad2fb0dee7bc398c2713d2cb7d092b4e5f
DeepsDali <deepashri.dali@gmail.com>
2023-07-09 23:14:35 +0100

    Update applyClassLists function to include isHardMode option
    Relates #5

--- JS/applyClasslist.js
+++ JS/applyClasslist.js
@@ -1,27 +1,30 @@
-export const applyClassLists = (randomWord) => {
+export const applyClassLists = async (randomWord, isHardMode) => {
   const gridContainer = document.getElementById("container");
   const allCells = Array.from(gridContainer.getElementsByClassName("cell"));
-
   console.log(randomWord);
   const currRowCells = allCells.filter((cell) =>
     cell.classList.contains("curr-row-cell")
   );
-  console.log("Curr row cells:", currRowCells);
-  console.log(`rw: ${randomWord}`);
+
   const splitRandom = randomWord.split("");
-  console.log(`splitw: ${splitRandom}`);
   const arrayOfCells = currRowCells.map((cell) => cell);
 
-  console.log("Curr row cells:", arrayOfCells);
-  console.log("All cells:", allCells);
   arrayOfCells.forEach((cell, i) => {
     const cellValue = cell.value ? cell.value.toUpperCase() : "";
     const randomChar = randomWord[i];
+
+    cell.classList.remove("matching-char", "correct-position");
+
     if (splitRandom.includes(cellValue)) {
-      cell.classList.add("matching-char");
+      cell.classList.add(
+        isHardMode ? "matching-char-hardMode" : "matching-char"
+      );
     }
+
     if (cellValue === randomChar) {
-      cell.classList.add("correct-position");
+      cell.classList.add(
+        isHardMode ? "correct-position-hardMode" : "correct-position"
+      );
     }
   });
 };
